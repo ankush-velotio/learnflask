53508c5e5474602654d0b0d634f527f53ed527bb
Ankush Chavan <ankush.chavan@velotio.com>
2022-01-04 17:57:20 +0530

    feat(Test): Structure the test environment for project
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from auth.views import auth
 from database import database
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
     # Setup with the provided configuration
     app.config.from_object("config.DevelopmentConfig")
+
+    if test_config is not None:
+        app.config.update(test_config)
 
     database.init_app(app)
 
